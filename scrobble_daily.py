17673224e7f1026c672072d1dc4ddf5cf1f9781f
katakingboom35 <katakingboom@gmail.com>
2026-09-08 20:49:48 +0300

    Let requests handle response compression

--- scrobble_daily.py
+++ scrobble_daily.py
@@ -230,8 +230,8 @@ def main():
     import_all = os.getenv("IMPORT_ALL_HISTORY", "0") == "1"
 
     browser_headers = parse_browser_headers(browser_json_raw or "")
-    browser_headers.pop("Content-Encoding", None)
-    browser_headers.pop("content-encoding", None)
+    for copied_header in ("Content-Encoding", "content-encoding", "Accept-Encoding", "accept-encoding"):
+        browser_headers.pop(copied_header, None)
     browser_headers.setdefault("x-youtube-bootstrap-logged-in", "true")
     browser_headers.setdefault("referer", "https://music.youtube.com/")
     browser_headers.setdefault("x-origin", "https://music.youtube.com")
